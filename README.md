a1f259c8d37f8f10b2261e9b24a0ef1874898d06
Gustavo Kumagai <iagamuk.gus@gmail.com>
2023-08-06 20:01:03 -0300

    Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+SISCARGA BM é um sistema de controle de verificação de veículos para corpo de bombeiros.
+
+Projeto de TCC de Gustavo Amamia Kumagai no Instituto Federal do Paraná em 2023
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Siscarga BM</title>
+</head>
+<body>
+  <h1>Olá Mundo!</h1>
+</body>
+</html>
