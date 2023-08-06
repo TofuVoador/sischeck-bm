aaf10ed22c5f4b09e2186f507a31a86c00f90257
Gustavo Kumagai <iagamuk.gus@gmail.com>
2023-08-06 20:43:09 -0300

    06/08/23

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
-<?php
-?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Siscarga BM</title>
-</head>
-<body>
-  <h1>Olá Mundo!</h1>
-</body>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="stylesheet" href="styles.css"/>
+  </head>
+  <body>
+    <header>
+      <img src="/assets/siscarga.png" alt="siscarga logo"/>
+      <h1 class="title">Siscarga BM</h1>
+    </header>
+    <section>
+      <main>
+          <img src="/assets/6sgbi.png" alt="brasão 6sgbi" />
+          <input class="input-user"><h1 class="title1">USUÁRIO</h1></input>
+          <input class="input-password"><h1 class="title1">SENHA</h1></input>
+          <button class="forgot-button"><h1 class="title">ESQUECI MINHA SENHA</h1></button>
+      </main>
+    </section>
+  </body>
 </html>
