4fd4c603e40d3e350bb156fe199aa5037018d5ac
Gustavo Kumagai <iagamuk.gus@gmail.com>
2023-12-07 18:12:11 -0300

    Quantidade de Materiais

--- veiculos/dados.php
+++ veiculos/dados.php
@@ -9,7 +9,10 @@ $sql = "SELECT * FROM veiculo where id = $idVeiculo";
 $result = $conn->query($sql);
 $veiculo = $result->fetch_assoc();
 
-$sql = "SELECT * FROM compartimento where idVeiculo = $idVeiculo";
+$sql = "SELECT c.*, count(mnv.id) as 'materiais' FROM compartimento as c
+        LEFT JOIN materiais_no_veiculo as mnv on mnv.idCompartimento = c.id
+        WHERE c.idVeiculo = $idVeiculo
+        GROUP BY c.id";
 $compartimentos = $conn->query($sql);
 ?>
 <!DOCTYPE html>
@@ -35,15 +38,20 @@ $compartimentos = $conn->query($sql);
         <input type="submit" value="Cadastrar" class="button">
       </form>
     </div>
-    <h1>Compartimentos de <?php echo $veiculo['prefixo'] . "-" . $veiculo['posfixo'] ?></h1>
-    <?php $last_compartimento = null;     
-    foreach ($compartimentos as $c) { ?>
-      <div class="secondary-section">
-        <h1><?= $c['nome'] ?></h1>
-        <a class="button" href="desativar_compartimento.php?id=<?= $c['id'] ?>">Desativar Compartimento</a>
-        <a class="button" href="alocacoes.php?id=<?= $c['id'] ?>">Alocações</a>
-      </div>
-    <?php } ?>
+    <div class="secondary-section">
+      <h1>Compartimentos</h1>
+      <?php $last_compartimento = null;     
+      foreach ($compartimentos as $c) { ?>
+        <div class="card">
+          <h1><?= $c['nome'] ?></h1>
+          <p><?= $c['materiais'] ?> materiais alocados</p>
+          <p>
+            <a class="button" href="desativar_compartimento.php?id=<?= $c['id'] ?>">Desativar</a>
+            <a class="button" href="alocacoes.php?id=<?= $c['id'] ?>">Alocações</a>
+          </p>
+        </div>
+      <?php } ?>
+    </div>
   </section>
   <section>
     
