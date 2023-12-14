e179a985dff7f683119d9726a6f63f9f61e6584b
Gustavo Kumagai <iagamuk.gus@gmail.com>
2023-12-14 17:39:34 -0300

    Quantidade Notificações

--- dashboard.php
+++ dashboard.php
@@ -29,10 +29,29 @@ if($_SESSION['usuario']['status'] != 'ativo') {
   <section>
     <main>
       <a class="button" href="./veiculos">Veículos<a>
-      <?php if ($usuario['tipo'] == 'administrador') { ?>
+      <?php if ($usuario['tipo'] == 'administrador') { 
+          require_once("conexao.php");
+
+          // Consulta para contar o número de problemas não resolvidos
+          $sql = "SELECT ch.id FROM check_mnv as ch
+                  LEFT JOIN (
+                    SELECT MAX(ch1.data_check) as max_data_check, mnv.id as mnv_id
+                    FROM check_mnv as ch1
+                    LEFT JOIN materiais_no_veiculo as mnv ON mnv.id = ch1.idMateriais_no_Veiculo
+                    WHERE mnv.status = 'ativo'
+                    GROUP BY mnv.id
+                  ) as ultima_verificacao ON ch.data_check = ultima_verificacao.max_data_check
+                  LEFT JOIN materiais_no_veiculo as mnv ON mnv.id = ch.idMateriais_no_Veiculo
+                  WHERE mnv.status = 'ativo' AND ch.resolvido = 0
+                    AND ch.data_check = ultima_verificacao.max_data_check
+                  GROUP BY mnv.id
+                  ORDER BY ch.data_check";
+
+          $result = $conn->query($sql);
+          $quantidadeNotif = $result->num_rows; ?>
         <a class="button" href="./materiais">Materiais<a>
         <a class="button" href="./setores">Setores<a>
-        <a class="button" href="./notificacoes">Notificações<a>
+        <a class="button" href="./notificacoes"><?php echo $quantidadeNotif != 0 ? '('.$quantidadeNotif.')' : '' ?> Notificações<a>
         <a class="button" href="./usuarios">Usuários<a>
       <?php } ?>
     </main>
