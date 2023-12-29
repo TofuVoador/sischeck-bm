0f9e6c234d07da1b5a94c4dfc5e5a56d9ef4232b
Gustavo Kumagai <iagamuk.gus@gmail.com>
2023-12-29 07:07:43 -0300

    SQL Injection

--- index.php
+++ index.php
@@ -6,24 +6,34 @@ if(isset($_POST['password']) && isset($_POST['user'])) {
   $login = $_POST['user'];
   $senha = $_POST['password'];
 
-  // Consulta SQL para verificar as credenciais and retrieve user type
-  $sql = "SELECT * FROM usuario WHERE login = '$login' AND senha = '$senha'";
-  $result = $conn->query($sql);
+  // Consulta SQL para verificar as credenciais
+  $sql = "SELECT * FROM usuario WHERE login = ? AND senha = ?";
+  $stmt = $conn->prepare($sql);
 
-  if ($result->num_rows > 0) {
-      // Fetch user data
-      $user = $result->fetch_assoc();
-      
-      // Store user type in session
-      session_start();
-      $_SESSION['usuario'] = $user;
-      
-      // Redirect to appropriate dashboard based on user type
-      header("Location: dashboard.php");
-  } else { 
-      echo "Nome de usuário ou senha incorretos.";
+  if($stmt) {
+    //Bind dos parâmetros
+    $stmt->bind_param('ss', $login, $senha);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // Fetch user data
+        $user = $result->fetch_assoc();
+        
+        // Store user type in session
+        session_start();
+        $_SESSION['usuario'] = $user;
+        
+        // Redirect to appropriate dashboard based on user type
+        header("Location: dashboard.php");
+    } else { 
+        echo "Nome de usuário ou senha incorretos.";
+    }
+  } else {
+    echo "Erro na preparação do SQL.";
   }
 
+  $stmt->close();
   $conn->close();
 }
 ?>
