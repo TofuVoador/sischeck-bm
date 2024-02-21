c9035cf9b9a6ff6a72a2aae2acdd2d67728c1a1c
Gustavo Kumagai <iagamuk.gus@gmail.com>
2024-02-21 07:28:46 -0300

    Redirecionamento

--- veiculos/desalocar.php
+++ veiculos/desalocar.php
@@ -25,5 +25,11 @@ require_once("../conexao.php");
 $sql = "UPDATE materiais_no_veiculo SET status = 'inativo' WHERE id = $idMNV";
 $conn->query($sql);
 
-header("Location: index.php");
+$sql = "SELECT c.id as 'idCompartimento' from materiais_no_veiculo as mnv
+        LEFT JOIN compartimento as c on c.id = mnv.idCompartimento
+        WHERE mnv.id = $idMNV";
+$result = $conn->query($sql);
+$idCompartimento = $result->fetch_assoc()['idCompartimento'];
+
+header("Location: dados.php?id=$idCompartimento");
 ?>
