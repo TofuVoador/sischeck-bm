838c4493dc221bbbcc4a94cf1ab646fd38c8b105
Gustavo <65035245+TofuVoador@users.noreply.github.com>
2024-03-18 15:18:01 -0300

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header>
   <div class="logo">
-    <a href="../"><img src="../assets/SisCheck-BM.png" alt="sischeck-bm logo" /></a>
+    <a href="../dashboard.php"><img src="../assets/SisCheck-BM.png" alt="sischeck-bm logo" /></a>
     <h1 class="logo-name">SisCheck-BM</h1>
   </div>
   <div>
